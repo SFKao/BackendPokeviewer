2cd17163d3d73678414e8d1656d42defcf9287ae
Oscar <oscarmeleroxd@gmail.com>
2023-01-16 13:17:48 +0100

    Actualizados pokemon

--- Actualizar pokemons.js
+++ Actualizar pokemons.js
@@ -2,17 +2,27 @@ const fs = require('fs');
 
 let obtenerPokemons = async() => {
     let pokemons = [];
-    for (let i = 1; i < 905; i++) {
-        let fet = await fetch('https://pokeapi.co/api/v2/pokemon/' + i);
-        if (fet == undefined)
-            return;
-        let poke = await fet.json();
-        let objeto = new Object();
-        objeto.name = poke.name;
-        objeto.id = poke.id;
-        objeto.img = poke.sprites.front_default;
-        objeto.imgS = poke.sprites.front_shiny;
-        pokemons.push(objeto);
+    for (let i = 1; i < 1200; i++) {
+        if(i%100 == 0)
+            console.log(i);
+        let fet;
+        let poke
+        try{
+        fet = await fetch('https://pokeapi.co/api/v2/pokemon/' + i);
+        poke = await fet.json();
+        }catch(e){
+            console.log(i);
+            fet = undefined;
+        }
+        if (fet != undefined){
+            let objeto = new Object();
+            objeto.name = poke.name;
+            objeto.id = poke.id;
+            objeto.img = poke.sprites.front_default;
+            objeto.imgS = poke.sprites.front_shiny;
+
+            pokemons.push(objeto);
+        }
     }
     console.log(JSON.stringify(pokemons));
     fs.writeFile("pokemons.json", JSON.stringify(pokemons), 'utf8', function(err) {
